a59c2ba376822b471de440ce65fc5db4dd1a3bfd
Matthew Ganaden <mjdganaden@gmail.com>
2024-09-18 20:45:14 +0800

    rename link from index to nextlink

--- src/components/header/nav/link/index.js
+++ src/components/header/nav/link/index.js
@@ -1,14 +1,14 @@
 import { motion } from 'framer-motion';
 import { slide } from '../../anim'
 import styles from './style.module.scss'
-import Link from 'next/link';
+import NextLink from 'next/link';
 
 export default function index({data}) {
   return (
     <motion.div custom={data.index} variants={slide} animate="enter" exit="exit" initial="initial" className={styles.link}>
-      <Link href={data.href}>
+      <NextLink href={data.href}>
           {data.title}
-      </Link>
+      </NextLink>
     </motion.div>
   )
 }
